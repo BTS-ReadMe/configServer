e002deda027f0008c6f653fe7d3bdae0a2407341
logwns <logwns@gmail.com>
2023-05-03 14:16:29 +0900

    [UPDATE] update config server

--- src/main/java/com/readme/configServer/controller/ConfigController.java
+++ src/main/java/com/readme/configServer/controller/ConfigController.java
@@ -8,9 +8,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/config")
 public class ConfigController {
 
-    @GetMapping
-    public String test() {
-        return "@@@@@@@@@@@@@@";
-    }
-
 }
